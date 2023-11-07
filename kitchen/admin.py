213329166f272e8b1e3eec7b7a137a7a978d09d0
Illia Zhmurskiy <thebrawler6@gmail.com>
2023-11-07 23:41:59 +0300

    create models.py and admin.py

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from kitchen.models import Cook, DishType, Dish
+
 
 # Register your models here.
+@admin.register(Cook)
+class CookAdmin(admin.ModelAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Years of experience", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Years of experience",
+                {
+                    "fields": (
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    search_fields = ("name", "price", "dish_type")
+    list_filter = ("price",)
+
+
+admin.site.register(DishType)
--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 from django.db import models
 
+
 # Create your models here.
+class DishType(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=1, validators=[
+        MinValueValidator(1)
+    ])
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook)
