a3d359d340aa419556ce03087876962a61c43878
Illia Zhmurskiy <thebrawler6@gmail.com>
2023-12-12 19:21:56 +0300

    modify setting.py and restaurant kitchen service urls.py

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import restaurant_kitchen_service.settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -45,6 +48,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap4",
     "kitchen",
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
